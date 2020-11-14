369d04ed2553ac3072c5bc22113f47ae6d571056
Mariia Randarevych <renij1986@gmail.com>
2020-11-15 01:31:04 +0300

    task-7 ready

---
+++ js/task-7.js
@@ -0,0 +1,6 @@
+const inpRef = document.getElementById('font-size-control')
+const textRef = document.getElementById('text')
+inpRef.addEventListener('input', changeSize)
+function changeSize(){
+textRef.style.fontSize= inpRef.value + 'px';
+}
