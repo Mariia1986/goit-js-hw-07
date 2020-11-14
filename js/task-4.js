03165c507be9817b87128af7c56e8833224ff806
Mariia Randarevych <renij1986@gmail.com>
2020-11-14 16:32:32 +0300

    task 4 ready

--- js/task-4.js
+++ js/task-4.js
@@ -1,10 +1,14 @@
-// Счетчик состоит из спана и кнопок,
-//  которые должны увеличивать и уменьшать
-//   значение счетчика на 1.
 
-// Создай переменную counterValue в которой будет 
-// хранится текущее значение счетчика.
-// Создай функции increment и decrement для 
-// увеличения и уменьшения значения счетчика
-// Добавь слушатели кликов на кнопки, вызовы 
-// функций и обновление интерфейса
+const btnDecRef = document.querySelector('[data-action="decrement"]')
+const btnIncRef=document.querySelector('[data-action="increment"]')
+const valueRef=document.getElementById('value')
+
+btnDecRef.addEventListener("click", handlerFuncDec);
+btnIncRef.addEventListener("click", handlerFuncInc);
+let counterValue=0;
+
+function handlerFuncDec(){
+    valueRef.textContent=counterValue >0 ? counterValue-=1 : 0;}
+
+    function handlerFuncInc(){
+    valueRef.textContent=counterValue+=1}
