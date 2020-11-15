02b0442eac8d4865367bc39c3d6fa3a35a216de3
Mariia Randarevych <renij1986@gmail.com>
2020-11-15 14:22:24 +0300

    task 6 edit

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,15 @@
 
 const inputRef = document.getElementById('validation-input')
 inputRef.addEventListener("blur", handlerInpReq)
+inputRef.addEventListener("keydown", handlerInp)
 function handlerInpReq() {
 inputRef.value.length === Number(inputRef.dataset.length) ? 
 inputRef.classList.add('valid') :
 inputRef.classList.add('invalid')
+// function handlerIn(){
+//     inputRef.classList.remove('valid','invalid')
+}
 
+function handlerInp(){
+    inputRef.classList.remove('valid','invalid')
 }
