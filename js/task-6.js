352fcec5e844be04510ab27d6205b64199ec4d0c
Mariia Randarevych <renij1986@gmail.com>
2020-11-15 16:46:16 +0300

    ready

--- js/task-6.js
+++ js/task-6.js
@@ -6,8 +6,7 @@ function handlerInpReq() {
 inputRef.value.length === Number(inputRef.dataset.length) ? 
 inputRef.classList.add('valid') :
 inputRef.classList.add('invalid')
-// function handlerIn(){
-//     inputRef.classList.remove('valid','invalid')
+
 }
 
 function handlerInp(){
